6319a5016acd3ac12a1ff636b70c88c7d58cdf17
Luis Cuzquen <cuzquenjr@gmail.com>
2019-02-18 18:47:50 -0700

    routing

--- src/components/main.js
+++ src/components/main.js
@@ -9,10 +9,10 @@ import Resume from './resume';
 const Main = ()=>(
     <Switch>
         <Route exact path="/myportfoliosite" component={LandingPage}/>
-        <Route path="/myportfoliosite//aboutme" component={AboutMe}/>
-        <Route path="/myportfoliosite//contact" component={Contact}/>
-        <Route path="/myportfoliosite//projects" component={Projects}/>
-        <Route path="/myportfoliosite//resume" component={Resume}/>
+        <Route path="/myportfoliosite/aboutme" component={AboutMe}/>
+        <Route path="/myportfoliosite/contact" component={Contact}/>
+        <Route path="/myportfoliosite/projects" component={Projects}/>
+        <Route path="/myportfoliosite/resume" component={Resume}/>
     </Switch>
 )
 
