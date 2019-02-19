fed14bf118c95fbc5d917f0e922411a432d34cbc
Luis Cuzquen <cuzquenjr@gmail.com>
2019-02-18 18:55:00 -0700

    Updates

--- precache-manifest.2d6e4de897e1dc49ac77af59f5ca29f2.js
+++ precache-manifest.2d6e4de897e1dc49ac77af59f5ca29f2.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/myportfoliosite/static/js/runtime~main.067ee2d3.js"
   },
   {
-    "revision": "b94daee5927239178ecc",
-    "url": "/myportfoliosite/static/js/main.b94daee5.chunk.js"
+    "revision": "3be89bff142426f79477",
+    "url": "/myportfoliosite/static/js/main.3be89bff.chunk.js"
   },
   {
     "revision": "a9c94b49950e4bb8c1ff",
     "url": "/myportfoliosite/static/js/1.a9c94b49.chunk.js"
   },
   {
-    "revision": "b94daee5927239178ecc",
+    "revision": "3be89bff142426f79477",
     "url": "/myportfoliosite/static/css/main.bff4f7a5.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/myportfoliosite/static/css/1.e120860a.chunk.css"
   },
   {
-    "revision": "b3436518003519a65b5d4900bcf2361a",
+    "revision": "33eedd45051f92d1add478a1af387fc8",
     "url": "/myportfoliosite/index.html"
   }
 ];
